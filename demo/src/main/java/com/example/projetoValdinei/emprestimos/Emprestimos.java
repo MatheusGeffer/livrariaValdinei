18d641bfe4cee2ea634876ea13fbea55c7558d07
Matheus Geffer <matheus.tg22@gmail.com>
2024-06-11 20:25:10 -0300

    fix: update class Emprestimos

--- demo/src/main/java/com/example/projetoValdinei/emprestimos/Emprestimos.java
+++ demo/src/main/java/com/example/projetoValdinei/emprestimos/Emprestimos.java
@@ -1,4 +1,17 @@
 package com.example.projetoValdinei.emprestimos;
 
+import com.example.projetoValdinei.livros.Livros;
+import jakarta.persistence.*;
+
+@Entity
 public class Emprestimos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "livro_id")
+    private Livros livros;
+
 }
